2529af78fe18b07b566758c47342be4d2a85e417
Marta Medio <martamedio@gmail.com>
2018-09-13 15:53:02 +0200

    Save messages and send existing ones to the client when joins

--- lib/chat_web/channels/meetup_channel.ex
+++ lib/chat_web/channels/meetup_channel.ex
@@ -2,6 +2,7 @@ defmodule ChatWeb.MeetupChannel do
   use ChatWeb, :channel
 
   def join("meetup:lobby", payload, socket) do
+    send(self(), :after_join)
     if authorized?(payload) do
       {:ok, socket}
     else
@@ -18,8 +19,18 @@ defmodule ChatWeb.MeetupChannel do
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (meetup:lobby).
   def handle_in("shout", payload, socket) do
+    Chat.Message.changeset(%Chat.Message{}, payload) |> Chat.Repo.insert
     broadcast socket, "shout", payload
     {:noreply, socket}
+  end
+
+  def handle_info(:after_join, socket) do 
+    Chat.Message.get_messages() 
+      |> Enum.each(fn msg -> push(socket, "shout", %{
+        name: msg.name,
+        message: msg.message
+      }) end)
+      {:noreply, socket}
   end
 
   # Add authorization logic here as required.
